7f4fc3d35843bf73048652e91698eec97ba25035
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2021-10-20 21:30:18 +0200

    Proto: add Entrypoint.pp and use it

--- src/proto_alpha/lib_plugin/plugin.ml
+++ src/proto_alpha/lib_plugin/plugin.ml
@@ -641,8 +641,9 @@ module View_helpers = struct
       ~pp:(fun ppf (entrypoint, typ) ->
         Format.fprintf
           ppf
-          "The view %s has type %a, it is not compatible with a TZIP-4 view \
+          "The view %a has type %a, it is not compatible with a TZIP-4 view \
            type."
+          Entrypoint.pp
           entrypoint
           Micheline_printer.print_expr
           (Micheline_printer.printable
@@ -662,8 +663,9 @@ module View_helpers = struct
       ~pp:(fun ppf (entrypoint, callback) ->
         Format.fprintf
           ppf
-          "The view %s never initiated a transaction to the given callback \
+          "The view %a never initiated a transaction to the given callback \
            contract %a."
+          Entrypoint.pp
           entrypoint
           Contract.pp
           callback)
@@ -681,9 +683,10 @@ module View_helpers = struct
       ~pp:(fun ppf (entrypoint, callback) ->
         Format.fprintf
           ppf
-          "The view %s initiated a list of operations while the TZIP-4 \
+          "The view %a initiated a list of operations while the TZIP-4 \
            standard expects only a transaction to the given callback contract \
            %a."
+          Entrypoint.pp
           entrypoint
           Contract.pp
           callback)
