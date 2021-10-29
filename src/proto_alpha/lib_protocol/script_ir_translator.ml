43f03241003be5030583871e1bedef5877c4ece4
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2021-10-29 23:34:18 +0200

    Proto: make Entrypoint.t private

--- src/proto_alpha/lib_protocol/script_ir_translator.ml
+++ src/proto_alpha/lib_protocol/script_ir_translator.ml
@@ -482,7 +482,7 @@ let unparse_timestamp ~loc ctxt mode t =
 
 let unparse_address ~loc ctxt mode (c, entrypoint) =
   Gas.consume ctxt Unparse_costs.contract >>? fun ctxt ->
-  (match entrypoint with
+  (match (entrypoint : Entrypoint.t :> string) with
   (* given parse_address, this should not happen *)
   | "" -> error Unparsing_invariant_violated
   | _ -> ok ())
@@ -2012,12 +2012,7 @@ let find_entrypoint (type full) (full : full ty) ~root_name entrypoint =
               | None -> None))
     | _ -> None
   in
-  let entrypoint =
-    if Compare.String.(entrypoint = "") then "default" else entrypoint
-  in
-  if Compare.Int.(String.length entrypoint > 31) then
-    error (Entrypoint.Name_too_long entrypoint)
-  else if field_annot_opt_eq_entrypoint_lax root_name entrypoint then
+  if field_annot_opt_eq_entrypoint_lax root_name entrypoint then
     ok ((fun e -> e), Ex_ty full)
   else
     match find_entrypoint full entrypoint with
